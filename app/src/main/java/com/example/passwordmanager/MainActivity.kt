bcd7b51cd7e8a653c7b5402ad51743be369a0c00
airamdl <airamdl2001@gmail.com>
2024-10-01 13:05:26 +0100

    first commit

--- app/src/main/java/com/example/passwordmanager/MainActivity.kt
+++ app/src/main/java/com/example/passwordmanager/MainActivity.kt
@@ -16,32 +16,7 @@ import com.example.passwordmanager.ui.theme.PasswordManagerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            PasswordManagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PasswordManagerTheme {
-        Greeting("Android")
-    }
-}
