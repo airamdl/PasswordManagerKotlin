3a062cfdb4a3a50348b590dae1e2dac17c84cae3
airamdl <airamdl2001@gmail.com>
2024-10-09 11:22:04 +0100

    Text Field

--- app/src/main/java/com/example/passwordmanager/MainActivity.kt
+++ app/src/main/java/com/example/passwordmanager/MainActivity.kt
@@ -7,91 +7,127 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.passwordmanager.ui.theme.PasswordManagerTheme
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             PasswordManagerTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { paddingValues -> Text(
-                    text = "Nombre: ",
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(paddingValues)
-                )
-                }
+                Scaffold(content = { padding ->
+                    PassUserInput()
+                })
             }
         }
     }
 }
 
-@Composable
-fun PasswordManager(){
-    Column(
-        modifier = Modifier
-            .padding(15.dp)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(15.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    )
-    {
-//        Text("Siguiente password")
-//        NextButtonExample(onClick =  { Log.d("Button", "Next Button clicked") })
+//@Composable
+//fun OnSubmit(){
+//    var submit by remember { mutableStateOf("") }
+//    TextField(
+//        value = submit,
+//        onValueChange = { newText ->
+//            submit = newText.trimStart { it == '0' }
+//        }
+//    )
+//
+//}
 
+@Composable
+@Preview
+fun PassUserInput() {
+    var title by rememberSaveable { mutableStateOf("") }
+    var user by rememberSaveable { mutableStateOf("") }
+    var pass by rememberSaveable { mutableStateOf("") }
+
+    var submit by remember { mutableStateOf("") }
+
+
+
+
+
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = title,
+            onValueChange = { title = it },
+            label = { Text("Introduce a title") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+        )
+
+        Spacer(modifier = Modifier.padding(10.dp))
+
+
+        TextField(
+            value = user,
+            onValueChange = { user = it },
+            label = { Text("Usuario") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+        )
+
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        TextField(
+            value = pass,
+            onValueChange = { pass = it },
+            label = { Text("Introce your password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
+
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        Text(
+            text = "Remember to check the password and edit if you need",
+            fontSize = 30.sp,
+            color = Color.LightGray
+        )
+
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        (Button(
+            onClick = onSubmit
+        )
+        {
+            Text(text = "Save Pass")
+        })
     }
 }
-
-
-
-
-//@Composable
-//fun NextButtonExample(onClick: () -> Unit) {
-//    NextButton(
-//        onClick = { onClick() }
-//    ) {
-//        Icon(
-//            Icons.Filled.PlayArrow,
-//            "next password",
-//            modifier = modifier.padding(12.dp),
-//            "blue"
-//
-//        )
-//        Text("Next Password")
-//
-//    }
-//}
-//
-//
-//@Composable
-//fun PrevButtonExample(onClick: () -> Unit) {
-//    PrevButton(
-//        onClick = { onClick() }
-//    ) {
-//        Icon(Icons.AutoMirrored.Filled.ArrowBack)
-//        Text("Previous Password")
-//    }
-//}
-//
-
-
 
 
 
